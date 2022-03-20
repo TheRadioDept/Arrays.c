c631066033d9299fbfdff5be8e549024e4dcf93a
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-03-20 17:23:48 +0400

    Create printMenu.c

---
+++ printMenu.c
@@ -0,0 +1,39 @@
+void printMenu(){
+    printf("You can choose one of these services: \n");
+    printf("1. Print matrix tabular form. \n");
+    printf("2. Print a specific row or a number of rows in sequence \n");
+    printf("3. Print a specific column or a number of columns in sequence \n");
+    printf("4. Get the elements of a specific row reversed \n"); 
+    printf("5. find the number of prime numbers in a specific row \n");
+    printf("6. Quit \n");
+    int answer = scanf("answer: ");
+    switch (answer)
+    {
+    case 1:
+        
+        break;
+    
+    case 2:
+        /* code */
+        break;
+
+    case 3:
+        /* code */
+        break;
+
+    case 4: 
+        /* code */ 
+        break; 
+
+    case 5: 
+        /* code */ 
+        break; 
+
+    case 6: 
+        printf("Bye!\n");
+        break; 
+
+    default:
+        break;
+    }
+}
